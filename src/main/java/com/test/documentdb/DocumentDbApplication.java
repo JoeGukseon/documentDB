aad0d080a474f8ec21fc9ab833a9183cf8a940f2
JoeGukSeon <zipcks1381@gmail.com>
2023-07-28 23:36:24 +0900

    DocumentDB 테스트용

--- src/main/java/com/test/documentdb/DocumentDbApplication.java
+++ src/main/java/com/test/documentdb/DocumentDbApplication.java
@@ -1,5 +1,7 @@
 package com.test.documentdb;
 
+import com.mongodb.client.*;
+import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DocumentDbApplication {
 
     public static void main(String[] args) {
+        String template = "mongodb://%s:%s@%s/sample-database?ssl=true&replicaSet=rs0&readpreference=%s";
+        String username = "<sample-user>";
+        String password = "<password>";
+        String clusterEndpoint = "sample-cluster.node.us-east-1.docdb.amazonaws.com:27017";
+        String readPreference = "secondaryPreferred";
+        String connectionString = String.format(template, username, password, clusterEndpoint, readPreference);
+
+        String truststore = "<truststore>";
+        String truststorePassword = "<truststorePassword>";
+
+        System.setProperty("javax.net.ssl.trustStore", truststore);
+        System.setProperty("javax.net.ssl.trustStorePassword", truststorePassword);
+
+        MongoClient mongoClient = MongoClients.create(connectionString);
+
+        MongoDatabase testDB = mongoClient.getDatabase("sample-database");
+        MongoCollection<Document> numbersCollection = testDB.getCollection("sample-collection");
+
+        Document doc = new Document("name", "pi").append("value", 3.14159);
+        numbersCollection.insertOne(doc);
+
+        MongoCursor<Document> cursor = numbersCollection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
+
         SpringApplication.run(DocumentDbApplication.class, args);
     }
 
